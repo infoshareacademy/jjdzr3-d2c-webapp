47664c0ce64f9b3849a6eed0ccd4cc562f5a403c
Artur Zabieglinski <AkuroNiariae@users.noreply.github.com>
2021-06-29 18:31:37 +0100

    Pagination alfa Version

--- src/main/java/com/d2c/webapp/Controller/DrinkListController.java
+++ src/main/java/com/d2c/webapp/Controller/DrinkListController.java
@@ -3,12 +3,18 @@ package com.d2c.webapp.Controller;
 import com.d2c.webapp.Service.DrinkService;
 import com.infoshareademy.domain.Drink;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class DrinkListController {
@@ -27,7 +33,23 @@ public class DrinkListController {
 
 
     @GetMapping(value = "/showAllDrinks2")
-    public String getShowAllDrinks2() {
+    public String getShowAllDrinks2(
+            Model model, @RequestParam("page")Optional<Integer>page, @RequestParam("size") Optional<Integer>size) {
+/*        List<Drink> drinkList = drinkService.getDrinkList();
+        model.addAttribute("Drinks", drinkList);*/
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(5);
+
+        Page<Drink> drinkPage = drinkService.findPaginated(PageRequest.of(currentPage-1,pageSize));
+
+        model.addAttribute("drinkPage", drinkPage);
+
+        int totalPages = drinkPage.getTotalPages();
+        if (totalPages>0){
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages).boxed().collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+
         return "subSites/showAllDrinks2";
     }
 
